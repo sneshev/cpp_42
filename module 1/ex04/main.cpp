5b8ca4809c728fbabc033138cafadb2245f02d1b
sneshev <steffal.n2@gmail.com>
2026-02-21 08:02:39 +0100

    working on ex04

--- module 1/ex04/main.cpp
+++ module 1/ex04/main.cpp
@@ -9,28 +9,55 @@
 	own tests to ensure that your program works as expected.
 */
 #include <iostream>
+#include <fstream>
+#include <string>
 #define str std::string
 #define BADARGC "Argument count must be 3 (file and 2 strings)"
-
-
-
+#define EMPTYSTR "Arguments can not be empty"
+#define BADFILE "Error: Could not open file"
+#define FILECREATE "Error: Could not create file"
 bool error(str message) {
-	std::cout << message << std::endl;
+	std::cerr << message << std::endl;
 	return (false);
 }
 
-bool is_valid_input(int argc, char *argv[], int *fd) {
-	(void)argv;
-	(void)fd;
+bool is_valid_input(int argc, char *argv[]) {
 	if (argc != 4) {
 		return error(BADARGC);
+	}
+	for (int i = 1; i < 4; i++) {
+		str s = argv[i];
+		if (s.empty())
+			return (error(EMPTYSTR));
 	}
 	return (true);
 }
 
 
+str replace_all(str &c, str s1, str s2) {
+	
+}
+
 int main(int argc, char *argv[]) {
-	int fd;
-	if (!is_valid_input(argc, argv, &fd))
+	if (!is_valid_input(argc, argv))
 		return (1);
+	
+	std::ifstream inFile(argv[1]);
+	if (!inFile) {
+		error(BADFILE);
+		return (1);
+	}
+	std::ofstream outFile(str(argv[1]) + ".replace");
+	if (!outFile) {
+		error(FILECREATE);
+		return (1);
+	}
+	std::string content( 
+		(std::istreambuf_iterator<char>(inFile)),
+		std::istreambuf_iterator<char>()
+	);
+    content = replace_all(content, argv[2], argv[3]);
+    outFile << content;
+    return 0;
+
 }
