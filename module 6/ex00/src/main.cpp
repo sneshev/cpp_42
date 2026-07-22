3a050d24d7d9232d41a227965fb40689aba98254
John Doe <steffal.n2@gmail.com>
2026-07-22 23:15:42 +0200

    ex00

--- module 6/ex00/src/main.cpp
+++ module 6/ex00/src/main.cpp
@@ -1,8 +1,19 @@
 #include "../inc/ScalarConverter.hpp"
 
 int main(int argc, char *argv[]) {
-	if (argc == 2)
+	if (argc == 2) {
 		ScalarConverter::convert(argv[1]);
+		return (0);
+	}
+	else {
+		std::string tests[] = { "0", "nan", "42.0f", "-inff", "+inf", "c", "2147483648", "-42"};
 
-	return (0);
+		for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+			std::cout << "=== Input: " << tests[i] << " ===" << std::endl;
+			ScalarConverter::convert(tests[i]);
+			std::cout << std::endl;
+		}
+	}
+
+	return 0;
 }
